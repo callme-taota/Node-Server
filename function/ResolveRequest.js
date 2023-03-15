9d6a4fc4447046548a519fb9e61750a337a5cd05
Taota <745103130@qq.com>
2023-03-15 15:47:03 +0800

    beta1.0.a9
    处理完了大部分对于热更新端口的程序，已经可以实现保存即可用，但还没加入对sql语句的验证，错误的sql语句会导致崩端。

---
+++ function/ResolveRequest.js
@@ -0,0 +1,55 @@
+import { Dosql } from "./AccessDataBase.js";
+import { File } from "./File.js";
+export async function RR(p,req){
+
+    let a = File.getAPIconfig();
+    let t = -1;
+    for(let i=0;i<a.length;i++){
+        if(a[i].port===p){
+            t=i;
+        }
+    }
+    if(t>=0){
+        let sql = a[t].sql;
+        let limit_flag= a[t].limit_flag;
+        let str = "";
+        let limit = "";
+    
+        let query=[];
+        if(a[t].keyValue!=="" || a[t].keyValue!==null){
+            query = a[t].keyValue.split(";");
+        }
+    
+        if(query.length > 1 || JSON.stringify(req.query)!=="{}"){
+            str = " where "
+            for(let j=0; j<query.length; j++) {
+                if(req.query[query[j]] && req.query[query[j]][0] === `"`){
+                    str += query[j]+ `=`+ req.query[query[j]] + ' ';
+                }else if(req.query[query[j]]){
+                    str += query[j]+ `="`+ req.query[query[j]] + '" ';
+                }
+            }
+            if(req.query.limit && !req.query.offset){
+                limit += ' limit '
+                limit += req.query.limit;
+            }
+            if(req.query.limit && req.query.offset){
+                limit += ' limit '
+                limit += (parseInt(req.query.offset)+1) + " , " + (parseInt(req.query.limit)+parseInt(req.query.offset) )
+            }
+        }
+    
+        if(str.length!==7) sql += str;
+        if(limit!=="" && limit_flag) sql += limit;
+        console.log(sql);
+        let res = await Dosql(sql);
+        let len = res.length;
+        let obj = {res,len}
+        return obj;
+    }else{
+        let obj = {}
+        return obj;
+    }
+    
+
+}
--- function/Server.js
+++ function/Server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { File } from './File.js';
 import { Dosql } from './AccessDataBase.js';
-import { Server } from '../app.js';
+import { RR } from './ResolveRequest.js';
 
 export const Router =express.Router();
 //getHTML path
@@ -33,38 +33,9 @@ export function StartServer(){
     for(let i = 0; i <API.length; i++) {
         let port = API[i].port;
         Router.get(port, async (req, res) => {
-            let sql = API[i].sql;
-            let limit_flag = API[i].limit_flag;
-
-            let query = [];
-            if(API[i].keyValue!=="" || API[i].keyValue!==null) {
-                query = API[i].keyValue.split(";")
-            }
-            let str =""
-            let limit = ""
-            if(query.length > 1 || JSON.stringify(req.query)!=="{}"){
-                str = " where "
-                for(let j=0; j<query.length; j++) {
-                    if(req.query[query[j]] && req.query[query[j]][0] === `"`){
-                        str += query[j]+ `=`+ req.query[query[j]] + ' ';
-                    }else if(req.query[query[j]]){
-                        str += query[j]+ `="`+ req.query[query[j]] + '" ';
-                    }
-                }
-                if(req.query.limit && !req.query.offset){
-                    limit += ' limit '
-                    limit += req.query.limit;
-                }
-                if(req.query.limit && req.query.offset){
-                    limit += ' limit '
-                    limit += (parseInt(req.query.offset)+1) + " , " + (parseInt(req.query.limit)+parseInt(req.query.offset) )
-                }
-            }
-            if(str.length!==7) sql += str;
-            if(limit!=="" && limit_flag) sql += limit;
-            console.log(sql);
+            let data = await RR(port,req);
             res.setHeader("Access-Control-Allow-Origin", "*")
-            res.send(await Dosql(sql))
+            res.send(data);
         })
     }
 } 
